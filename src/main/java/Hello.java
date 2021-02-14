abb3b493bbcfbaf8cc08ce4316687776d6083c6a
ozkankrm <ozkankerim20@gmail.com>
2021-02-14 15:51:53 +0000

    master branch, spouse changed the movie location

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world B22");
         System.out.println("EU4 something good");
     }
 }
