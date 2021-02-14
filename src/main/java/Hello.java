ff7cafdb2ef6ffe753573e6a944a97d3cb0fffbd
ozkankrm <ozkankerim20@gmail.com>
2021-02-14 16:27:12 +0000

    movie2 branch watching on the phone for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world B22");
         System.out.println("EU4 something good");
+
+        //movie on the phone
     }
 }
