8a923d1f808f9b303e93253811c9ab9b2c742052
ozkankrm <ozkankerim20@gmail.com>
2021-02-14 15:37:14 +0000

    movie 1 branch updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world, EU4");
         System.out.println("EU4 something good");
     }
 }
